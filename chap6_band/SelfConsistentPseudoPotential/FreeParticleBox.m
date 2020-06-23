72bdbfe8fd9dfd2d5330f56578057d8f970e057d
WuzhangFang <wfang@huskers.unl.edu>
2020-06-23 11:48:55 -0500

    free particle in box

--- chap6_band/SelfConsistentPseudoPotential/FreeParticleBox.m
+++ chap6_band/SelfConsistentPseudoPotential/FreeParticleBox.m
@@ -3,11 +3,25 @@ L = 5;
 b = 2 * pi / L;
 
 % basis sets
-K = [0 0 0; b 0 0; -b 0 0; 0 b 0; 0 -b 0; 0 0 b; 0 0 -b];
+K = [0 0 0; 
+     b 0 0; 
+     -b 0 0; 
+     0 b 0; 
+     0 -b 0; 
+     0 0 b; 
+     0 0 -b];
 
 H = zeros(7,7);
 for i=1:7
     H(i,i) = norm(K(i,:))^2/2;
 end
 
-E = eig(H);
+[V, E] = eig(H);
+disp(sum(E));
+
+% calcualte the density (6.70)
+
+Omega = L^3; % volume
+
+% occupancy
+f1 = 2; f2 = 1/3;
